f3470df429d836d0eee75ba752550eac0e7322da
Ashutosh Pawar <ashutoshpawar07@gmail.com>
2022-05-07 11:28:22 +0530

    Update button.js

--- button.js
+++ button.js
@@ -1,4 +1,4 @@
 const button = document.createElement('button')
-button.innerHTML = "click there"
+button.innerHTML = "click there now or else it wont be added"
 const section = document.getElementById('shopify-section-hero-1')
 section.appendChild(button)
